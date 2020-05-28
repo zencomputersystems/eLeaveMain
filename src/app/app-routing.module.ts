d36146cae75f57e0c3f47383bdbb23b53eee39f3
Hanani Rafei <hanani@zen.com.my>
2020-05-28 15:28:08 +0800

    update module aliases based on root/admin/user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { SampleApp1SharedModule } from './projects/sampleapp1/src/app/app.module';
-import { AppModule } from 'admin/app/app.module';
 // import { SideMenuUserSharedModule } from './projects/user/src/app/side-menu-navigation/side-menu-navigation.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
-  // {
-  //   path: 'testPage',
-  //   loadChildren: '../app/projects/sampleapp1/src/app/app.module.ts#SampleApp1SharedModule'
-  // },
   {
     path: 'login',
-    // loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-    loadChildren: () => import('../app/projects/user/src/app/app.module').then(m => m.AppModule)
+    loadChildren: () => import('./projects/user/src/app/app.module').then(m => m.AppModule)
+    // loadChildren: () => import('$user-root/src/app/app.module').then(m => m.AppModule)
   },
   {
     path: 'users', loadChildren: () => import('./projects/user/src/app/app.module').then(m=>m.AppModule)
-    // path: 'users', loadChildren: '../app/projects/user/src/app/app.module.ts'
-  },
-  // {
-  //   path: 'adminusers', loadChildren: () => import('../app/projects/user/src/app/app.module').then(m=>m.AppModule)
-  // },
-  // {
-  //   path: 'testadmin', loadChildren: () => import('./projects/admin/src/app/admin1/admin1.module').then(m=>m.Admin1PageModule)
-  // },
+    // path: 'users', loadChildren: () => import('$user-root/src/app/app.module').then(m=>m.AppModule)
+   },
   {
     path: 'admins', loadChildren: () => import('./projects/admin/src/app/app.module').then(m=>m.AppModule)
+    // path: 'admins', loadChildren: () => import('$admin-root/src/app/app.module').then(m=>m.AppModule)
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true }),
-    SampleApp1SharedModule.forRoot(),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
   ],
   exports: [RouterModule]
 })
